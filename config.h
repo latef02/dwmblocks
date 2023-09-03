c2e7f364ef70c1235a6358d3a93885e5271b425f
latef02 <milanjack2@gmail.com>
2023-09-03 15:00:54 +0100

    second

--- config.h
+++ config.h
@@ -4,14 +4,14 @@ static const Block blocks[] = {
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   
   {"",  "packages",  18000,  16}, 
-  {"",  "checkupdate",  18000,  16},
+/*  {"",  "checkupdate",  18000,  16},*/
   {"",  "memory", 2,  10},
-  {"",  "internet",  2, 1},
+  {"",  "internet",  10, 1},
   {"",  "cpu", 5,  4},
   {"",  "clocks", 60,  4},
-  {"",  "xback", 0,  16},
+  {"",  "xback", 1,  16},
   {"",  "batterychecks", 10,  4},
-  {"",  "vol", 0,  16},
+  {"",  "vol", 1,  16},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
