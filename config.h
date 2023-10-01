b6c38cf6fbbdaa3c75bc3e967d0b46693b36afed
latef02 <milanjack2@gmail.com>
2023-10-01 01:49:31 +0100

    pol

--- config.h
+++ config.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   
   {"",  "packages",  18000,  16}, 
- /* {"",  "checkupdate",  18000,  16},*/
+  {"",  "checkupdate",  18000,  16},
   {"",  "memory", 2,  10},
   {"",  "internet",  10, 1},
   {"",  "cpu", 5,  4},
@@ -12,9 +12,8 @@ static const Block blocks[] = {
   {"",  "clocks", 60,  4},
   {"",  "xback", 1,  16},
   {"",  "batterychecks", 10,  4},
-  {"",  "vol", 1,  16}
-
-};
+  {"",  "vol", 1,  16},
+ };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
